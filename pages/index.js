006f370490e29939fafd1220a3b888d860415749
Nurbek-Abdulhamidov <abdulhamidnurbek@gmail.com>
2022-10-19 13:29:13 +0500

    navbar is finished

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import ArticleList from "../components/ArticleList";
 
-export default function Home() {
-  return <div>
-    <h1>Welcome to Next</h1>
-  </div>;
+export default function Home({ articles }) {
+  console.log(articles);
+  return (
+    <div>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta name="keywords" content="web development , programmning" />
+      </Head>
+      <ArticleList articles={articles} />
+    </div>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+  return {
+    props: {
+      articles,
+    },
+  };
+};
